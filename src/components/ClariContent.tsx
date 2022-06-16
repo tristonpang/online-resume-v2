73d15d96d3e616c01b0be5fb6ad9c295f18f65fa
Triston Pang <triston@tempest.com>
2022-06-16 17:43:28 +0800

    Add image carousel

--- src/components/ClariContent.tsx
+++ src/components/ClariContent.tsx
@@ -1,4 +1,8 @@
+import '../../node_modules/react-image-gallery/styles/css/image-gallery.css';
+
 import React from 'react';
+
+import ImageGallery from 'react-image-gallery';
 
 import {
   Box,
@@ -7,6 +11,8 @@ import {
 } from '@material-ui/core';
 
 import { clariContent } from '../data/clariContent';
+import FilmLaughPhoto from '../resources/images/claritris_filmlaugh_50.jpg';
+import UmbrellaPhoto from '../resources/images/claritris_umbrella_50.jpg';
 import Section from './Section';
 
 const useStyles = makeStyles(() => createStyles({
@@ -24,14 +30,33 @@ const useStyles = makeStyles(() => createStyles({
     paddingRight: '20%',
     paddingTop: '10%',
     paddingBottom: '10%',
+  },
+  gallery: {
+    width: '100%'
   }
 }));
+
+const images = [
+  {
+    original: UmbrellaPhoto,
+    thumbnail: UmbrellaPhoto,
+    originalHeight: 200,
+    thumbnailHeight: 50,
+  },
+  {
+    original: FilmLaughPhoto,
+    thumbnail: FilmLaughPhoto,
+    originalHeight: 200,
+    thumbnailHeight: 50
+  }
+];
 
 const ClariContent = () => {
   const classes = useStyles();
   return (
     <Box className={classes.container}>
       {/* TODO: add picture scroller here */}
+      <ImageGallery additionalClass={classes.gallery} items={images}/>
       <Section id='content' sectionTitle='This is Clari...' content={clariContent} useEssayCards />
     </Box>
   );
