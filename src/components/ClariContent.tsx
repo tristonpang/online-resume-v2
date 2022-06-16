dd9b3b94be8925cfccde20be789ba42f073487ee
Triston Pang <triston@tempest.com>
2022-06-16 21:42:55 +0800

    Add more photos

--- src/components/ClariContent.tsx
+++ src/components/ClariContent.tsx
@@ -11,8 +11,29 @@ import {
 } from '@material-ui/core';
 
 import { clariContent } from '../data/clariContent';
+import AlphabetPhoto from '../resources/images/claritris_alphabet.jpeg';
+import BlackWhitePhoto from '../resources/images/claritris_blackwhite.jpeg';
+import BrunchPhoto from '../resources/images/claritris_brunch.jpeg';
+import CameraPhoto from '../resources/images/claritris_camera.jpeg';
+import ChijmesPhoto from '../resources/images/claritris_chijmes.jpeg';
+import ChristmasPhoto from '../resources/images/claritris_christmas.jpeg';
+import DoughPhoto from '../resources/images/claritris_dough.jpeg';
 import FilmLaughPhoto from '../resources/images/claritris_filmlaugh_50.jpg';
+import FirstPhoto from '../resources/images/claritris_first.jpeg';
+import GbtbPhoto from '../resources/images/claritris_gbtb.jpeg';
+import BdayPhoto from '../resources/images/claritris_umbrella_50.jpg';
 import UmbrellaPhoto from '../resources/images/claritris_umbrella_50.jpg';
+// import GradPhoto from '../resources/images/claritris_grad.jpeg';
+// import LungsPhoto from '../resources/images/claritris_lungs.jpeg';
+// import MilligramPhoto from '../resources/images/claritris_milligram.jpeg';
+// import PotteryPhoto from '../resources/images/claritris_pottery.jpeg';
+// import Reflection1Photo from '../resources/images/claritris_reflection1.jpeg
+// ';
+// import Reflection2Photo from '../resources/images/claritris_reflection2.jpeg';
+// import Reflection3Photo from '../resources/images/claritris_reflection3.jpeg';
+// import Reflection4Photo from '../resources/images/claritris_reflection4.jpeg';
+// import SelfiePhoto from '../resources/images/claritris_selfie.jpeg';
+// import SelfieNaturePhoto from '../resources/images/claritris_selfienature.jpeg';
 import Section from './Section';
 
 const useStyles = makeStyles(() => createStyles({
@@ -32,7 +53,8 @@ const useStyles = makeStyles(() => createStyles({
     paddingBottom: '10%',
   },
   gallery: {
-    width: '100%'
+    width: '100%',
+    marginBottom: '10%'
   }
 }));
 
@@ -40,15 +62,75 @@ const images = [
   {
     original: UmbrellaPhoto,
     thumbnail: UmbrellaPhoto,
-    originalHeight: 200,
+    originalHeight: 500,
     thumbnailHeight: 50,
   },
   {
     original: FilmLaughPhoto,
     thumbnail: FilmLaughPhoto,
-    originalHeight: 200,
+    originalHeight: 500,
     thumbnailHeight: 50
-  }
+  },
+  {
+    original: AlphabetPhoto,
+    thumbnail: AlphabetPhoto,
+    originalHeight: 500,
+    thumbnailHeight: 50
+  },
+  {
+    original: BdayPhoto,
+    thumbnail: BdayPhoto,
+    originalHeight: 500,
+    thumbnailHeight: 50
+  },
+  {
+    original: BlackWhitePhoto,
+    thumbnail: BlackWhitePhoto,
+    originalHeight: 500,
+    thumbnailHeight: 50
+  },
+  {
+    original: BrunchPhoto,
+    thumbnail: BrunchPhoto,
+    originalHeight: 500,
+    thumbnailHeight: 50
+  },
+  {
+    original: CameraPhoto,
+    thumbnail: CameraPhoto,
+    originalHeight: 500,
+    thumbnailHeight: 50
+  },
+  {
+    original: ChijmesPhoto,
+    thumbnail: ChijmesPhoto,
+    originalHeight: 500,
+    thumbnailHeight: 50
+  },
+  {
+    original: ChristmasPhoto,
+    thumbnail: ChristmasPhoto,
+    originalHeight: 500,
+    thumbnailHeight: 50
+  },
+  {
+    original: DoughPhoto,
+    thumbnail: DoughPhoto,
+    originalHeight: 500,
+    thumbnailHeight: 50
+  },
+  {
+    original: FirstPhoto,
+    thumbnail: FirstPhoto,
+    originalHeight: 500,
+    thumbnailHeight: 50
+  },
+  {
+    original: GbtbPhoto,
+    thumbnail: GbtbPhoto,
+    originalHeight: 500,
+    thumbnailHeight: 50
+  },
 ];
 
 const ClariContent = () => {
@@ -56,7 +138,7 @@ const ClariContent = () => {
   return (
     <Box className={classes.container}>
       {/* TODO: add picture scroller here */}
-      <ImageGallery additionalClass={classes.gallery} items={images}/>
+      <ImageGallery additionalClass={classes.gallery} items={images} autoPlay />
       <Section id='content' sectionTitle='This is Clari...' content={clariContent} useEssayCards />
     </Box>
   );
--- src/data/clariContent.ts
+++ src/data/clariContent.ts
@@ -1,3 +1,28 @@
+// import AlphabetPhoto from '../resources/images/claritris_alphabet.jpeg';
+// import BlackWhitePhoto from '../resources/images/claritris_blackwhite.jpeg';
+// import BrunchPhoto from '../resources/images/claritris_brunch.jpeg';
+// import CameraPhoto from '../resources/images/claritris_camera.jpeg';
+// import ChijmesPhoto from '../resources/images/claritris_chijmes.jpeg';
+// import ChristmasPhoto from '../resources/images/claritris_christmas.jpeg';
+// import DoughPhoto from '../resources/images/claritris_dough.jpeg';
+// import FilmLaughPhoto from '../resources/images/claritris_filmlaugh_50.jpg';
+// import FirstPhoto from '../resources/images/claritris_first.jpeg';
+// import GbtbPhoto from '../resources/images/claritris_gbtb.jpeg';
+// import BdayPhoto from '../resources/images/claritris_umbrella_50.jpg';
+// import UmbrellaPhoto from '../resources/images/claritris_umbrella_50.jpg';
+
+// import GradPhoto from '../resources/images/claritris_grad.jpeg';
+// import LungsPhoto from '../resources/images/claritris_lungs.jpeg';
+// import MilligramPhoto from '../resources/images/claritris_milligram.jpeg';
+// import PotteryPhoto from '../resources/images/claritris_pottery.jpeg';
+// import Reflection1Photo from '../resources/images/claritris_reflection1.jpeg
+// ';
+// import Reflection2Photo from '../resources/images/claritris_reflection2.jpeg';
+// import Reflection3Photo from '../resources/images/claritris_reflection3.jpeg';
+// import Reflection4Photo from '../resources/images/claritris_reflection4.jpeg';
+// import SelfiePhoto from '../resources/images/claritris_selfie.jpeg';
+// import SelfieNaturePhoto from '../resources/images/
+
 export const clariContent = [
   {
     title: '...someone I hold so dear to my heart',
@@ -12,6 +37,20 @@ export const clariContent = [
     Nam enim ligula, porta vel enim vitae, hendrerit faucibus ante. Aliquam erat volutpat. Nam at sapien dui. Fusce non dictum libero. Nullam neque felis, finibus sit amet sapien sed, consectetur consequat quam. Duis in nisi sed justo condimentum convallis. Duis turpis dolor, mattis in urna sit amet, aliquam eleifend mauris. Praesent aliquam nisl vel elit rutrum, ac laoreet dolor rutrum. Maecenas laoreet suscipit turpis, nec pellentesque diam porta in. Suspendisse facilisis nunc quis urna auctor iaculis. Morbi vehicula suscipit magna, et efficitur orci volutpat eget. Praesent malesuada mattis risus, nec porta risus rutrum in. Donec in ultrices quam, non laoreet tellus. Maecenas egestas pellentesque placerat. Cras eget lectus in mauris consectetur maximus at vel augue. Quisque ut pharetra dolor.
     
     Cras sit amet massa ac ipsum interdum scelerisque. Nullam faucibus mi est, a pharetra lacus laoreet sed. Quisque ullamcorper augue eget dolor bibendum, eu sodales nulla pellentesque. Sed bibendum consequat dui tincidunt dictum. Nulla a ante tincidunt, tincidunt mauris nec, congue eros. Nunc rhoncus ipsum ac malesuada tempus. Nunc condimentum felis ac ante ornare rhoncus. Quisque sit amet mauris tempus, venenatis ligula ac, dapibus odio. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-    `
+    `,
+    // galleryItems: [
+    //   AlphabetPhoto, 
+    //   BlackWhitePhoto,
+    //   BrunchPhoto,
+    //   CameraPhoto,
+    //   ChijmesPhoto,
+    //   ChristmasPhoto,
+    //   DoughPhoto,
+    //   FilmLaughPhoto,
+    //   FirstPhoto,
+    //   GbtbPhoto,
+    //   BdayPhoto,
+    //   UmbrellaPhoto,
+    // ],
   }
 ];
